5858ecdf0ab5dc549aa4bee8f8865d6fb2da8774
Your Name <youremail@domain.com>
2022-02-27 09:36:42 +0530

    create interface registation page & join firebase

---
+++ public/index.js
@@ -0,0 +1,14 @@
+const app = firebase.app()
+
+const cred = app._delegate._options;
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+
+const _app = initializeApp(cred)
+console.log(_app); 
+// let db = firebase.firestore()
+
+// console.log('====================================');
+// console.log(db);
+// console.log('====================================');
+
